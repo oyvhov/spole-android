b0206c6a018a4b353a0c8e7dacdcf6d40f1e4c74
oyvhov <oyvhov@gmail.com>
2026-09-16 13:24:41 +0200

    Release hero update as beta06

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -41,8 +41,8 @@ android {
         applicationId = "app.reelstack"
         minSdk = 26
         targetSdk = 36
-    versionCode = 84
-    versionName = "0.17.0-beta05"
+    versionCode = 85
+    versionName = "0.17.0-beta06"
 
         testInstrumentationRunner = "app.reelstack.SpoleTestRunner"
         vectorDrawables.useSupportLibrary = true
